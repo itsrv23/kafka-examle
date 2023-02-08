50cefd2c35dc5ae68cd0b4fb1fe84ade75cc8e0a
itsrv23 <101412457+itsrv23@users.noreply.github.com>
2023-02-08 16:07:45 +0300

    manual create topic

---
+++ src/main/java/kafka/config/KafkaConfiguration.java
@@ -0,0 +1,17 @@
+package kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic messageTopic() {
+        return TopicBuilder.name("message.topic")
+                .partitions(5)
+                .build();
+    }
+}
